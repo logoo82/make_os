733b0cb297f49c1e395cb24e9ca03e8c071b6648
logoo82 <logoo82@naver.com>
2026-01-13 02:27:36 +0900

    add header FIle

--- 01.Kernel32/Source/Main.c
+++ 01.Kernel32/Source/Main.c
@@ -1,15 +1,25 @@
+#include "Types.h"
+
+void kPrintString(int iX, int iY, const char* pcString);
+
 void Main()
 {
-    int line = 5;
-    char str[9] = "C Kernel!";
-    char* video = (char*)(0xB8000 + 160*line);
-
-    for(int i = 0; str[i] != 0; i++)
-    {   
-        *video++ = str[i];
-        *video++ = 0x0A;
-    }
+    kPrintString(0, 4, "C Kernel!!");
 
     while(1);
+}
 
+//문자열 출력 함수
+void kPrintString(int iX, int iY, const char* pcString)
+{
+    CHARACTER* pstScreen = (CHARACTER*) (0xB8000 + (iY*160) + (iX*2));
+
+
+    int i;
+
+        for(i = 0; pcString[i] != 0; i++)
+    {
+        pstScreen[i].b_character = pcString[i];
+        pstScreen[i].b_attribute = 0x0A;
+    }
 }
